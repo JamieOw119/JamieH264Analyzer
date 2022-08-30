1f835464d781a56ac3b2b9a05c27192da5ce0419
JamieOw119 <1139937387@qq.com>
2022-08-30 19:21:21 +0800

    adjustment

--- Application/include/global.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GLOBAL_H_
-#define _GLOBAL_H_
-
-typedef unsigned char UINT8;
-typedef unsigned int UINT32;
-typedef unsigned short UINT16;
-
-#endif
--- Application/src/SimpleH264Analyzer.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/Stream.h"
-
-int main(int argc, char const *argv[])
-{
-    CStreamFile h264Stream(const_cast<char *>(argv[1]));
-    h264Stream.Parse_h264_bitstream();
-    return 0;
-}
